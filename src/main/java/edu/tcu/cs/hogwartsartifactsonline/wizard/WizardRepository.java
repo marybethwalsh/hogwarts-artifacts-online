483c077b21251668271f6f885c1d9af9d56b1962
marybethwalsh <112439768+marybethwalsh@users.noreply.github.com>
2024-03-10 03:02:15 -0500

    Add files via upload

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/wizard/WizardRepository.java
@@ -0,0 +1,7 @@
+package edu.tcu.cs.hogwartsartifactsonline.wizard;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface WizardRepository extends JpaRepository<Wizard, Integer> {
+}
